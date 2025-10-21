f3520b9fce5a6e20589e522773d971328db2103b
Marco <161529566+marcopileci@users.noreply.github.com>
2025-10-21 14:10:16 -0300

    Add files via upload

--- Python II/Clase 1/Los Pollos Hermanos Program/LPH.py
+++ Python II/Clase 1/Los Pollos Hermanos Program/LPH.py
@@ -1,3 +1,4 @@
+import time
 def verificar_nombre(nombre):
     while True:
         if nombre.isalpha():
@@ -8,7 +9,6 @@ def verificar_nombre(nombre):
             print("Por favor, ingrese un nombre de manera valida.")
             nombre= input("Ingresa el nombre: ")
 
-                
 def verificar_numero(numero):
     while True:
             if numero.isdigit(): 
@@ -19,82 +19,88 @@ def verificar_numero(numero):
                 numero= input("Ingrese un numero para el pedido: ")
 
 def cambio_de_turno(empleado):
-    empleado = input("ingrese Nombre Del encargado ")
+    empleado = input("Ingrese el nombre del encargado: ")
     return empleado
 
-encargado = input("Ingrese Nombre Del encargado: ")
+def registro_ingreso(empleado):
+    hora= time.strftime("%d-%m-%Y %H:%M:%S", time.localtime())
+    registro= f"IN {hora} - {empleado} ha Ingresado.\n"
+    with open("registro.txt", "a") as f:
+        f.write(registro)
+
+def registro_salida(empleado, ventas):
+    hora= time.strftime("%d-%m-%Y %H:%M:%S", time.localtime())
+    registro= f"OUT {hora} - {empleado} se ha retidado. Ventas: ${ventas}\n"
+    with open("registro.txt", "a") as f:
+        f.write(registro)
+###ventas##
+ventas_totales_encargado_actual= 0
+
+encargado = input("Ingrese el nombre del encargado: ")
 encargado= verificar_nombre(encargado)
+registro_ingreso(encargado)
+
 while True:
-    print(f"Welcome back {encargado} To The Los Pollos Hermanos Family")
+    print(f"Welcome back {encargado} to Los Pollos Hermanos Family")
     print("1. Ingresa Nuevo pedido")
     print("2. Cambio de turno")
     print("3. Apagar Sistema")
     opcion = input(">>> ")
     match opcion:
         case "1":
-            #############################################################################################
-            nombre= input("ingrese el nombre del cliente: ")
-            verificar_nombre(nombre)
-
+            #########################
+            # Ingresar nuevo pedido #
+            #########################
+            nombre= input("Ingrese el nombre del cliente: ")
+            nombre= verificar_nombre(nombre)
             combo1 = verificar_numero(input("Ingrese cantidad de Combo 1: "))
-
             combo2 = verificar_numero(input("Ingrese cantidad de Combo 2: "))
-
             combo3 = verificar_numero(input("Ingrese cantidad de Combo 3: "))
-
             postre = verificar_numero(input("Ingrese cantidad de Postre: "))
-
             PrecioCombo1 = 7
-
             PrecioCombo2 = 3
-
             PrecioCombo3 = 5
-
             PrecioPostre = 4
-
             totalCostoCombo1 = PrecioCombo1 * combo1
-
             totalCostoCombo2 = PrecioCombo2 * combo2
-
             totalCostoCombo3 = PrecioCombo3 * combo3
-
             totalCostoPostre = PrecioPostre * postre
-
-
             total= totalCostoCombo1 + totalCostoCombo2 + totalCostoCombo3 + totalCostoPostre
-
-            print("el precio total del pedido es de $" , total)
-
+            print("El precio total del pedido es de $" , total)
             while True:
-    
                 paga= verificar_numero(input("Abona con $"))
                 if paga < total:
-                    print("¡¡¡ FALTA DINERO PARA COMPRAR EL PEDIDO !!!")
+                    print("No se puede continuar con el pedido si se abona con un monto menor al total.")
                 else:
                     break    
             vuelto= paga - total
-
-            print("el vuelto para el cliente es de $" , vuelto)
-
-
+            print("El vuelto para el cliente es de $",vuelto)
             while True:
-
                 FinPedido=  input("¿Confirma pedido? Y/N : ").lower()
                 match FinPedido:
                     case "y":
-                        print(" ¡¡ Pedido Guardado !!") 
-            
+                        ventas_totales_encargado_actual= ventas_totales_encargado_actual + total
+                        hora= time.strftime("%d-%m-%Y %H:%M:%S", time.localtime())
+                        ventas= f"{nombre} , {hora} , {combo1} , {combo2}, {combo3}, {postre}, ${total}\n"
+                        with open("ventas.txt", "a") as f:
+                            f.write(ventas)
+                        print("¡Pedido Guardado!")
                         break 
                     case "n":
-                        print("¡¡ Pedido Eliminado !!")
+                        print("¡Pedido Eliminado!")
                         break
                     case _:
-                        print("ERROR , Confirme el pedido con Y o no con N ")
+                        print("Confirme el pedido presionando ""Y""(Si) o ""N""(No).")
         case "2":
+            # Cambio de turno
+            registro_salida(encargado, ventas_totales_encargado_actual)
+            ventas_totales_encargado_actual= 0
             encargado= cambio_de_turno(encargado)
             encargado= verificar_nombre(encargado)
+            registro_ingreso(encargado)
         case "3":
-            print("¡¡Gracias por Trabajar en Los Pollos Hermanos!!")
+            registro_salida(encargado, ventas_totales_encargado_actual)
+            print("Gracias por Trabajar en Los Pollos Hermanos!")
             break
         case _:
-            print("Opcion Invalida")
+            print("Opcion Invalida.")
