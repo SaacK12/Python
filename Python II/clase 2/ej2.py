bcdd51ee991396575ffc8b0edcdd48e3b9cb5c0f
Marco <161529566+marcopileci@users.noreply.github.com>
2025-10-23 19:30:56 -0300

    Add files via upload

---
+++ Python II/clase 2/ej2.py
@@ -0,0 +1,19 @@
+while True:
+    try:
+        a= float(input("Introduzca el primer numero: "))
+        b= float(input("Introduzca el segundo numero: "))
+    except ValueError:
+        print("El valor que ha introducido no es un numero valido.")
+    else:
+        break
+suma= a + b
+print(f"La suma de {a} + {b} es: {suma}")
+resta= a + b
+print(f"Su resta da como resultado: {resta}")
+multiplicar= a * b
+print(f"Su multiplicacion: {multiplicar}")
+try:
+    division= a / b
+    print(f"Y su division da: {division}")
+except ZeroDivisionError:
+    print("La division no es posible de realizarla por que el segundo numero es un cero.")
